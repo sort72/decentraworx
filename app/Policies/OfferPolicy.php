503b54181d406954f6ba44775592124e1873da2e
sort72 <saoa814@gmail.com>
2023-06-07 16:38:27 -0500

    Fix

--- app/Policies/OfferPolicy.php
+++ app/Policies/OfferPolicy.php
@@ -39,7 +39,8 @@ class OfferPolicy
      */
     public function update(User $user, Offer $offer): bool
     {
-        return $offer->company_id == $user->id || $user->type == 'admin';
+        return $user->type == 'company' || $user->type == 'admin';
+        // return $offer->company_id == $user->id || $user->type == 'admin';
     }
 
     /**
